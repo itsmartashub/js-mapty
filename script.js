1adbb91a39801adedf8e7c8cdff2a411e4db6de7
itsmartashub <coderawii@gmail.com>
2021-12-16 14:06:36 +0100

    ADDITIONAL FEATURE IDEAS

--- script.js
+++ script.js
@@ -339,3 +339,18 @@ const app = new App();
 //! !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 //! KADA KONVERTUJEMO OBJEKAT U STRING, PA POTOM STRING U OBJEKAT (KAO RECIMO ZA LOCAL STORAGE: JSON.STRINGIFY(), JSON.PARSE()), GUBIMO PROTOTYPE CHAIN, ZATO RECIMO ONAJ PUBLIC METOD CLICKS() NE RADI. ZATO POSTOJE PROBLEMI KAD SE RADI SA LOCALSTORAGE I OOP JAVASCRIPTOM
 //! !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+/*
+//? ADDITIONAL FEATURE IDEAS: CHALLENGES
+
+//* 1. Ability to edit a workout
+//* 2. Ability to delete a workout
+//* 3. Ability to delete all workouts
+//* 4. Ability to sort workouts by a certain field (e.g. distance)
+//* 5. Re-build Running and Cycling object coming from LS
+//* 6. More realistic error and confirmation messages
+//* 7. Ability to position the map to show all workouts [very hard]
+//* 8. Ability to draw lines and shapes instead of just points [very hard]
+//* 9. Geocode location from coordinates ("Run in Faro, Portugal") [only for async JS section]
+//* 10. Display weather data for workout time and place [only for async JS section]
+*/
